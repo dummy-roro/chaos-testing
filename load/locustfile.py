22e880dfd9f553c52eb3c129736b084218aef6a4
Dummy Robot <myatsoeaumg@hotmail.com>
2025-10-21 16:31:13 +0630

    Add locustfile for load testing VM application
    Implement load testing for order views and creation

---
+++ load/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class VMAppUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def view_orders(self):
+        self.client.get("http://<VM_IP_OR_DOMAIN>/orders")
+
+    @task
+    def create_order(self):
+        self.client.post("http://<VM_IP_OR_DOMAIN>/orders", json={"item": "laptop", "qty": 1})
